90c2d43d8b075bf3c9998ce030c4926d6004dc93
Deepak Sandhu <goncharovakiev@gmail.com>
2025-02-25 19:04:01 -0800

    Longest sequence function

---
+++ LISbigger.py
@@ -0,0 +1,12 @@
+n=[1,6,2,45,4,90,98,101,8,9,104,230,34,30,400]
+def LISbigger(i,j):
+    if j>len(n)-1:
+        return 0
+    elif i>=j:
+        return LISbigger(i,j+1)
+    else:
+        skip=LISbigger(i,j+1)
+        take=LISbigger(j,j+1)+1
+        return max(skip,take)
+
+print(LISbigger(0,0))
