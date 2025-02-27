3a530404b4343e42f7f8de7b4963bf8ee3d91288
Deepak Sandhu <goncharovakiev@gmail.com>
2025-02-26 22:06:11 -0800

    update

--- LISbigger.py
+++ LISbigger.py
@@ -2,12 +2,13 @@ n=[1,6,2,45,4,90,98,101,8,9,104,230,34,30,400]
 def LISbigger(i,j):
     if j>len(n)-1:
         return 0
-    skip=LISbigger(i,j+1)
+    elif n[i]>=n[j]:
+        return LISbigger(i,j+1)
 
-    take=0
-    if n[i]>n[j]:
-        take=LISbigger(j,j+1)+1
+    else:
+        skip= LISbigger(i,j+1)
+        take= LISbigger(j,j+1)+1
     
     return max(skip,take)
 
-print(LISbigger(0,0))
+print(LISbigger(0,1))
